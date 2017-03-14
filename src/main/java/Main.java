8006ce1bde9ff3fba5ac688f3361d7c01a4ec758
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-14 22:10:58 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        salary(700);
+    }
+
+    public static void salary(int a) {
+        a = a + 100;{
+        }
+        System.out.print("Твоя зарплата составляет:"+" "+ a + " "+"долларов в месяц.");
+    }
+}
